b168a30278bd49516501c884242867d8b3c9df30
ProtonStar12 <priyankabamaniya310@gamil.com>
2025-07-26 13:02:09 +0530

    fix2

--- types/kinde-auth.d.ts
+++ types/kinde-auth.d.ts
@@ -1,4 +1,5 @@
 declare module '@kinde-oss/kinde-auth-nextjs/server';
 declare module "@kinde-oss/kinde-auth-nextjs";
 declare module "@kinde-oss/kinde-auth-nextjs/components";
+declare module "@kinde-oss/kinde-auth-nextjs/middleware";
 
