e3acc8bc0e49d02ed07deabfe4ed8a32a8d6bfe6
ProtonStar12 <priyankabamaniya310@gamil.com>
2025-07-26 12:36:06 +0530

    fixed

--- app/action.ts
+++ app/action.ts
@@ -16,12 +16,15 @@ export async function handleSubmit(formdata: FormData) {
 const title = formdata.get('title');
 const content = formdata.get('content');
 const url = formdata.get('url');
+if (!title || !content || !url) {
+  throw new Error("Missing required fields");
+}
 
     const data = await prisma.blogPost.create ({
         data: {
-            title: title,
-            content: content,
-            imageUrl: url,
+            title: title as string,
+            content: content as string,
+            imageUrl: url as string ,
             authorId: user.id,
             authorName: user.given_name,
             authorImageUrl: user.picture, // Assuming you want to use the user's picture as the author image URL
