ee70262f82f121b4c2cf72f7867ada972c71f7b8
ProtonStar12 <priyankabamaniya310@gamil.com>
2025-07-26 20:32:38 +0530

    fix4

--- components/general/navbar.tsx
+++ components/general/navbar.tsx
@@ -1,12 +1,13 @@
+"use client";
 import Link from "next/link";
 import { Button, buttonVariants } from "../ui/button";
 import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
 
-export default async function Navbar(){
+export default  function Navbar(){
 
-    const {getUser} = getKindeServerSession();
-    const user = await getUser();
+    const {getUser} = useKindeBrowserClient();
+    const user =  getUser();
 return(
     <nav className="py-5 flex items-center justify-between">
 
